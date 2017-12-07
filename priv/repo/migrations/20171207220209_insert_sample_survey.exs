549266a862b378f88aaebd4d4517eaadc15beca8
kamidev <jonas.beckman@gmail.com>
2017-12-07 23:57:53 +0100

    Change migration

--- priv/repo/migrations/20171207220209_insert_sample_survey.exs
+++ priv/repo/migrations/20171207220209_insert_sample_survey.exs
@@ -5,7 +5,7 @@ defmodule SurveyAPI.Repo.Migrations.InsertSampleSurvey do
     execute """
     INSERT INTO surveys VALUES (
     1, 
-    'Sample seed survey', 
+    'Sample one-question survey', 
     1, 
     '{"pages": [{"title": "Sharing Cities - Product Survey", "questions": [{"name": "satisfaction", "type": "rating", "title": "How satisfied are you with the Product?", "maximumRateDescription": "Perfect", "mininumRateDescription": "Unsatisfied"}], "showProgressBar": "top"}]}', '2017-12-07', '2017-12-07');
     """
