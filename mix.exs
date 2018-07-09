3889d2ef0c20f3a19ceac320fe00ecf5552769ce
kamidev <jonas.beckman@gmail.com>
2018-07-09 13:58:03 +0200

    Remove migrations creating sample data to simplify testing

--- mix.exs
+++ mix.exs
@@ -54,7 +54,12 @@ defmodule SurveyAPI.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: [
+        "ecto.drop --quiet",
+        "ecto.create --quiet",
+        "ecto.migrate",
+        "test"
+      ]
     ]
   end
 end
