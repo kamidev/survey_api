c76ee140d7f53d66c4a6e6cec555d3a653e4cb5a
kamidev <jonas.beckman@gmail.com>
2018-07-25 23:47:37 +0200

    Require Elixir >= 1.7

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SurveyAPI.Mixfile do
     [
       app: :survey_api,
       version: "0.0.3",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
