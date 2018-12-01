5ae0d5f300f22d89aa042c0e09a2d871aafe00d3
kamidev <jonas.beckman@gmail.com>
2018-12-01 02:53:54 +0100

    Update Erlang to 21.1.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SurveyAPI.Mixfile do
     [
       app: :survey_api,
       version: "1.0.6",
-      elixir: "~> 1.7.1",
+      elixir: "~> 1.7.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
