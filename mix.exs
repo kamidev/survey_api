e93c1c28f5a97f53d0ee1905ec04508b0bcca86a
kamidev <jonas.beckman@gmail.com>
2019-03-13 18:57:45 +0100

    Update Erlang to 21.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SurveyAPI.Mixfile do
   def project do
     [
       app: :survey_api,
-      version: "1.3.2",
+      version: "1.3.3",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
