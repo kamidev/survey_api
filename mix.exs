4a55575310b139376a2cbbfc82e696bc116840c2
kamidev <jonas.beckman@gmail.com>
2019-03-31 20:42:41 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SurveyAPI.Mixfile do
   def project do
     [
       app: :survey_api,
-      version: "1.3.4",
+      version: "1.3.5",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
