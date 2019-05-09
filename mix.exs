b063c457d88832d5ebae4f058be478f1030152dc
kamidev <jonas.beckman@gmail.com>
2019-05-09 11:07:18 +0200

    Update phoenix

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SurveyAPI.Mixfile do
   def project do
     [
       app: :survey_api,
-      version: "1.4.3",
+      version: "1.4.4",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
@@ -33,11 +33,11 @@ defmodule SurveyAPI.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.4"},
+      {:phoenix, "~> 1.4.5"},
       {:phoenix_pubsub, "~> 1.1.2"},
       {:ecto, "~> 3.1.4"},
       {:ecto_sql, "~> 3.1.1"},
-      {:postgrex, "~> 0.14.2"},
+      {:postgrex, "~> 0.14.3"},
       {:phoenix_ecto, "~> 4.0.0"},
       {:gettext, "~> 0.16.1"},
       {:cors_plug, "~> 2.0.0"},
