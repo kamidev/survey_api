3890f4c4b25895eef83bbacd21317f0cc987f93d
kamidev <jonas.beckman@gmail.com>
2019-11-27 15:56:07 +0100

    Bump Erlang & Elixir to latest, new minor version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SurveyAPI.Mixfile do
   def project do
     [
       app: :survey_api,
-      version: "1.9.7",
+      version: "1.9.8",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
