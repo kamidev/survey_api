82949a4994d47f438fc228dd70796e89c66f771a
kamidev <jonas.beckman@gmail.com>
2020-04-06 06:00:49 +0200

    New minor release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SurveyAPI.Mixfile do
   def project do
     [
       app: :survey_api,
-      version: "1.12.2",
+      version: "1.12.3",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
