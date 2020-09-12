b01dac6a0a4f2daaa7b790b760b20f0d3299e2f6
kamidev <jonas.beckman@gmail.com>
2020-09-12 14:29:37 +0200

    Pre-release Elixir 11 version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SurveyAPI.Mixfile do
   def project do
     [
       app: :survey_api,
-      version: "1.17.2",
+      version: "1.18.0-pre",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
