42699381341a4f65ec7bbfc99fd217cfc7579c7e
kamidev <jonas.beckman@gmail.com>
2020-10-05 16:59:15 +0200

    New release (using Elixir RC version)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SurveyAPI.Mixfile do
   def project do
     [
       app: :survey_api,
-      version: "1.18.2-rc0",
+      version: "1.18.2",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
