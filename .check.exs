765eb4d6444623a48011ae80a9b4fcb6ac34720a
kamidev <jonas.beckman@gmail.com>
2021-03-26 09:19:13 +0100

    Disable ex_check doctor tool

--- .check.exs
+++ .check.exs
@@ -7,6 +7,7 @@
   tools: [
     ## curated tools may be disabled (e.g. the check for compilation warnings)
     {:compiler, false},
+    {:doctor, false},
 
     ## Run Credo using default settings. Display results in compact format.
     {:credo, "mix credo --format oneline"},
