e3eebc47d934759ae854235d813020ebe033b6f3
kamidev <jonas.beckman@gmail.com>
2021-12-12 00:50:59 +0100

    Use latest Postgres (14.1) in CircleCI pipeline
    See https://circleci.com/developer/images/image/cimg/postgres

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SurveyAPI.Mixfile do
   def project do
     [
       app: :survey_api,
-      version: "1.5.1",
+      version: "1.5.2",
       elixir: "~> 1.13.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
