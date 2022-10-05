d420f2ea0a4e7eb7ae7aa14ba708aca9497796a4
kamidev <1655+kamidev@users.noreply.github.com>
2022-10-05 10:39:07 +0200

    Works with Erlang 25.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SurveyAPI.Mixfile do
   def project do
     [
       app: :survey_api,
-      version: "1.11.0",
+      version: "1.11.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
